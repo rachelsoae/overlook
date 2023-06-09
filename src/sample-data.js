5b5c7f03136e15e90cf6391878f2cad3ded58fc2
Rachel Prather <rachelprather1@gmail.com>
2023-06-09 13:04:51 -0700

    refactor: Move assertion results to sample data for test readability

--- src/sample-data.js
+++ src/sample-data.js
@@ -49,6 +49,60 @@ const bookings = [
   }
 ]
 
+const bookings1 = [
+  {
+    "id": "1180820",
+    "userID": 1,
+    "date": "2018/08/20",
+    "roomNumber": 2
+  },
+  {
+    "id": "1160422",
+    "userID": 1,
+    "date": "2016/04/22",
+    "roomNumber": 4
+  },
+  {
+    "id": "1211004",
+    "userID": 1,
+    "date": "2021/10/04",
+    "roomNumber": 2
+  },
+  {
+    "id": "1220422",
+    "userID": 1,
+    "date": "2022/04/22",
+    "roomNumber": 3
+  }
+]
+
+const bookings5 = [
+  {
+    "id": "5131202",
+    "userID": 5,
+    "date": "2013/12/02",
+    "roomNumber": 4
+  },
+  {
+    "id": "5190629",
+    "userID": 5,
+    "date": "2019/06/29",
+    "roomNumber": 1
+  },
+  {
+    "id": "5230501",
+    "userID": 5,
+    "date": "2023/05/01",
+    "roomNumber": 2
+  },
+  {
+    "id": "5200311",
+    "userID": 5,
+    "date": "2020/03/11",
+    "roomNumber": 1
+  }
+]
+
 const rooms = [
   {
     "number": 1,
@@ -99,4 +153,4 @@ const customers = [
   }
 ]
 
-export { bookings, rooms, customers }
+export { bookings, bookings1, bookings5, rooms, customers }
--- test/bookings-test.js
+++ test/bookings-test.js
@@ -1,74 +1,26 @@
 import chai from 'chai';
 const expect = chai.expect;
-import { bookings, rooms, customers } from '../src/sample-data'
+import { bookings, bookings1, bookings5, rooms, customers } from '../src/sample-data'
 import { getBookings, getTotalCost } from '../src/bookings'
 
-const customer1 = customers[0];
-const customer2 = customers[1];
-const customer3 = customers[2];
-const allBookings = bookings;
-
 describe('Bookings per customer', () => {
+  const customer1 = customers[0];
+  const customer2 = customers[1];
+  const customer3 = customers[2];
+  const allBookings = bookings;
+  const bookingsA = bookings1;
+  const bookingsB = bookings5;
+  
   it('Should list all user\'s previous bookings', () => {
     const customer1Bookings = getBookings(allBookings, customer1)
     
-    expect(customer1Bookings).to.deep.equal([
-      {
-        "id": "1180820",
-        "userID": 1,
-        "date": "2018/08/20",
-        "roomNumber": 2
-      },
-      {
-        "id": "1160422",
-        "userID": 1,
-        "date": "2016/04/22",
-        "roomNumber": 4
-      },
-      {
-        "id": "1211004",
-        "userID": 1,
-        "date": "2021/10/04",
-        "roomNumber": 2
-      },
-      {
-        "id": "1220422",
-        "userID": 1,
-        "date": "2022/04/22",
-        "roomNumber": 3
-      }
-    ]);
+    expect(customer1Bookings).to.deep.equal(bookingsA);
   });
 
   it('Should be able to list bookings for a different user', () => {
     const customer2Bookings = getBookings(allBookings, customer2)
 
-    expect(customer2Bookings).to.deep.equal([
-      {
-        "id": "5131202",
-        "userID": 5,
-        "date": "2013/12/02",
-        "roomNumber": 4
-      },
-      {
-        "id": "5190629",
-        "userID": 5,
-        "date": "2019/06/29",
-        "roomNumber": 1
-      },
-      {
-        "id": "5230501",
-        "userID": 5,
-        "date": "2023/05/01",
-        "roomNumber": 2
-      },
-      {
-        "id": "5200311",
-        "userID": 5,
-        "date": "2020/03/11",
-        "roomNumber": 1
-      }
-    ]);
+    expect(customer2Bookings).to.deep.equal(bookingsB);
   });
 
   it('Should return a message if the user has no bookings', () => {    
@@ -79,6 +31,10 @@ describe('Bookings per customer', () => {
 });
 
 describe('Cost per customer', () => {
+  const customer1 = customers[0];
+  const customer2 = customers[1];
+  const customer3 = customers[2];
+  const allBookings = bookings;
   const allRooms = rooms;
 
   it('Should be able to calculate the total cost of bookings for a user', () => {
@@ -105,3 +61,9 @@ describe('Cost per customer', () => {
     expect(customer3Cost).to.equal(0);
   })
 });
+
+describe('Search avaialable rooms', () => {
+  it.skip('Should return a list of rooms available on a given date', () => {
+
+  })
+})
