89e2719f048117822b48d5707f565d6af6dc9106
Rachel Prather <rachelprather1@gmail.com>
2023-06-09 14:53:14 -0700

    refactor: Display message if no bookings/no rooms

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -22,31 +22,36 @@ const setDashboard = (customer, allBookings, allRooms) => {
   })
   
   const usersBookings = getBookings(allBookings, customer);
-  const spent = getTotalCost(allRooms, usersBookings);
+  
+  if (typeof usersBookings === 'string') {
+    yourBookingsCost.innerHTML = `<p>${usersBookings}</p>`
+  } else {
+    const spent = getTotalCost(allRooms, usersBookings);
 
-  yourBookingsCost.innerHTML = '';
-  yourBookings.innerHTML = '';
+    yourBookingsCost.innerHTML = '';
+    yourBookings.innerHTML = '';
 
-  yourBookingsCost.innerHTML = `$${spent}`
-
-  usersBookings.forEach(booking => {
-    let room = allRooms.find(room => room.number === booking.roomNumber);
-    let image = setRoomImage(room);
-    let bidet = checkForBidet(room);
-
-    yourBookings.innerHTML += 
-    `
-    <article class="room">
-      <h3 class="room-type">${room.roomType} ${bidet}</h3>  
-      <p class="booking-date">${booking.date}</p>
-      <img class="room-image" src=${image} alt="photo of a ${room.roomType}">
-      <div class="room-details">
-        <span class="material-icons-round">bed</span>  
-        <p class="num-beds">${room.numBeds} ${room.bedSize}</p>
-     </div>
-    </article>
-    `
-  })
+    yourBookingsCost.innerHTML = `$${spent}`
+    
+    usersBookings.forEach(booking => {
+      let room = allRooms.find(room => room.number === booking.roomNumber);
+      let image = setRoomImage(room);
+      let bidet = checkForBidet(room);
+  
+      yourBookings.innerHTML += 
+      `
+      <article class="room">
+        <h3 class="room-type">${room.roomType} ${bidet}</h3>  
+        <p class="booking-date">${booking.date}</p>
+        <img class="room-image" src=${image} alt="photo of a ${room.roomType}">
+        <div class="room-details">
+          <span class="material-icons-round">bed</span>  
+          <p class="num-beds">${room.numBeds} ${room.bedSize}</p>
+       </div>
+      </article>
+      `
+    })
+  }
 }
 
 const setRoomImage = (room) => {
@@ -70,34 +75,42 @@ const checkForBidet = (room) => {
 }
 
 const displaySearchResults = (allBookings, allRooms, date) => {
-  toggleHidden(dashboard)
-  toggleHidden(searchResults)
+  hide(dashboard)
+  show(searchResults)
 
   const availRooms = searchByDate(allBookings, allRooms, date);
-
   availableRooms.innerHTML = '';
-  availRooms.forEach(room => {
-    const image = setRoomImage(room);
-    const bidet = checkForBidet(room);
-    const cost = room.costPerNight.toFixed(2)
-    
-    availableRooms.innerHTML +=
-    `
-    <article class="room">
-      <h3 class="room-type">${room.roomType} ${bidet}</h3>  
-      <img class="room-image" src=${image} alt="photo of a ${room.roomType}">
-      <div class="room-details">
-        <span class="material-icons-round">bed</span>  
-        <p class="num-beds">${room.numBeds} ${room.bedSize}</p>
-      </div>
-      <p class="cost">$${cost} per night</p>
-    </article>
-    `
-  }) 
+
+  if (typeof availRooms === 'string') {
+    availableRooms.innerHTML = `<p>${availRooms}</p>`
+  } else {
+    availRooms.forEach(room => {
+      const image = setRoomImage(room);
+      const bidet = checkForBidet(room);
+      const cost = room.costPerNight.toFixed(2)
+      
+      availableRooms.innerHTML +=
+      `
+      <article class="room">
+        <h3 class="room-type">${room.roomType} ${bidet}</h3>  
+        <img class="room-image" src=${image} alt="photo of a ${room.roomType}">
+        <div class="room-details">
+          <span class="material-icons-round">bed</span>  
+          <p class="num-beds">${room.numBeds} ${room.bedSize}</p>
+        </div>
+        <p class="cost">$${cost} per night</p>
+      </article>
+      `
+    }) 
+  }
 }
 
-const toggleHidden = (element) => {
-  element.classList.toggle('hidden')
+const hide = (element) => {
+  element.classList.add('hidden');
+}
+
+const show = (element) => {
+  element.classList.remove('hidden');
 }
 
 
