2e32d3e19339e1c04ea25a5cbd513092b2342fd5
Rachel Prather <rachelprather1@gmail.com>
2023-06-11 16:19:30 -0700

    fix: Delete console.logs

--- src/api-calls.js
+++ src/api-calls.js
@@ -36,7 +36,6 @@ const bookRoom = (userID, selectedDate, roomNumber) => {
   })
   .then(response => checkForError(response))
   .then(response => getBookingsData())
-  .then(data => console.log(data))  
   .catch(error => alert(`${error.message}`))
 };
 
